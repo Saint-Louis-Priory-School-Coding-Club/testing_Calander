6b7fa8c6530a8da298c68762727b5c035a54c92a
crazytaj <43589239+crazytaj@users.noreply.github.com>
2020-01-18 11:58:29 -0600

    filestructure creation
    adds required filestructure to allow for more dynamic use of Laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('headers')
+<title>{{config('app.name', 'TC')}}</title>
+@endsection
+
+@section('content')
+<h1>Welcome to Laravel</h1>
+<p>This is a basic layout of the framework "Laravel"</p>
+@endsection
