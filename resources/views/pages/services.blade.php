404d30d9a89b4756d3ae7113ffec54ee20b496fa
Hex <35177464+Hexbugman213@users.noreply.github.com>
2020-01-25 16:50:08 -0600

    day date

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,11 +4,18 @@
     <div class="container-fluid">
         <div class="calander">
             <div class="row">
-                <div class="col-md border bg-light">this</div>
-                <div class="col-md border bg-light">is</div>
-                <div class="col-md border bg-light">it</div>
-                <div class="col-md border bg-light">chief</div>
-                <div class="col-md border bg-light">!!!</div>
+                <div class="col-md border bg-light">
+                    <div class="day-date">
+                        <b>testest</b>
+                    </div>
+                    <div class="day-content">
+                        testest
+                    </div>
+                </div>
+                <div class="col-md border bg-light"><b>Mon 1/27</b></div>
+                <div class="col-md border bg-light"><b>Mon 1/27</b></div>
+                <div class="col-md border bg-light"><b>Mon 1/27</b></div>
+                <div class="col-md border bg-light"><b>Mon 1/27</b></div>
             </div>
             <div class="row">
                 <div class="col-md border bg-light">this</div>
