e599ae2982f9661f54ebea076482f13692b470e9
Hex <35177464+Hexbugman213@users.noreply.github.com>
2020-01-25 16:28:44 -0600

    reformatting

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 @extends('layouts.nav')
 @section('content')
-<div class="container px-lg-5">
-  <div class="row py-5 mx-lg-n5">
-    <!-- <div class="col mx-auto border bg-light">this</div>
-    <div class="col mx-auto border bg-light">is</div>
-    <div class="col mx-auto border bg-light">it</div>
-    <div class="col mx-auto border bg-light">chief</div>
-    <div class="col mx-auto border bg-light">!!!</div> -->
-    <div class="col-md border bg-light">this</div>
-    <div class="col-md border bg-light">is</div>
-    <div class="col-md border bg-light">it</div>
-    <div class="col-md border bg-light">chief</div>
-    <div class="col-md border bg-light">!!!</div>
-  </div>
-</div>
+    <div class="container-fluid">
+        <div class="calander">
+            <div class="row">
+                <div class="col-md border bg-light">this</div>
+                <div class="col-md border bg-light">is</div>
+                <div class="col-md border bg-light">it</div>
+                <div class="col-md border bg-light">chief</div>
+                <div class="col-md border bg-light">!!!</div>
+            </div>
+            <div class="row">
+                <div class="col-md border bg-light">this</div>
+                <div class="col-md border bg-light">is</div>
+                <div class="col-md border bg-light">it</div>
+                <div class="col-md border bg-light">chief</div>
+                <div class="col-md border bg-light">!!!</div>
+            </div>
+        </div>
+    </div>
 @endsection
