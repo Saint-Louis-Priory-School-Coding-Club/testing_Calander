829e0c3fb4b55693cefe9cdba53c2050412f709d
CedricBruges <cbruges@stlprioryschool.org>
2020-01-25 16:09:34 -0600

    Update services.blade.php
    basic columns

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -3,11 +3,16 @@
 @section('content')
 <div class="container px-lg-5">
   <div class="row py-5 mx-lg-n5">
-    <div class="col py-3 px-lg-5 mx-auto border bg-light">this</div>
+    <!-- <div class="col py-3 px-lg-5 mx-auto border bg-light">this</div>
     <div class="col py-3 px-lg-5 mx-auto border bg-light">is</div>
     <div class="col py-3 px-lg-5 mx-auto border bg-light">it</div>
     <div class="col py-3 px-lg-5 mx-auto border bg-light">chief</div>
-    <div class="col py-3 px-lg-5 mx-auto border bg-light">!!!</div>
+    <div class="col py-3 px-lg-5 mx-auto border bg-light">!!!</div> -->
+    <div class="col-md py-3 px-lg-5 border bg-light">this</div>
+    <div class="col-md py-3 px-lg-5 border bg-light">is</div>
+    <div class="col-md py-3 px-lg-5 border bg-light">it</div>
+    <div class="col-md py-3 px-lg-5 border bg-light">chief</div>
+    <div class="col-md py-3 px-lg-5 border bg-light">!!!</div>
   </div>
 </div>
 @endsection
