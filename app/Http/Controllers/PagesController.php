ee61bb68ae0d85ae6c88dec42e1c6921c183b57f
hexcodefff <minecrafthex213@gmail.com>
2020-02-29 16:10:43 -0600

    add login

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PagesController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('pages.index');
     }
-    public function about() {
+
+    public function about()
+    {
         return view('pages.about');
     }
-    public function services() {
+
+    public function services()
+    {
         return view('pages.services');
+    }
+
+    public function login()
+    {
+        return view('login.services');
     }
 }
