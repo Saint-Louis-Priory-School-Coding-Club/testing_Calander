9528ceb756dcbf639ce1b50f71af5c2965557ab9
Rcarlin1027 <44122725+Rcarlin1027@users.noreply.github.com>
2020-02-29 17:28:57 -0600

    hbbbbbbb
    jgjhgjjhjg

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ Route::get('/', 'PagesController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
