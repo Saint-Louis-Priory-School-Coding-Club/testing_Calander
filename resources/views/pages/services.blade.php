bede32b88c8c44c50ac3a375a74d4841f1860f63
CedricBruges <cbruges@stlprioryschool.org>
2020-02-29 16:31:49 -0600

    asdf

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @extends('layouts.nav')
 @section('content')
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
     <div class="container-fluid">
         <div class="calander">
             <div class="row border">
